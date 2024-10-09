b9861f1d19f3ed13b65ca1349da6360fe48ff091
shoccho <sfb.shoccho@gmail.com>
2024-10-09 19:48:02 +0600

    Task 01:  added -l flag

--- 01-wc-tool/main.go
+++ 01-wc-tool/main.go
@@ -8,21 +8,29 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	flags := map[string]string{"-c": "bytes"}
-	process(args, flags)
+	flags := map[string]string{"-c": "bytes", "-l": "lines"}
+	fmt.Println(process(args, flags))
 }
 
-func process(args []string, flags map[string]string) {
+func process(args []string, flags map[string]string) string {
+
+	var output string
 	if value, ok := flags[args[0]]; ok {
 		file := processFile(args[1])
 		switch value {
 		case "bytes":
-			{
-				fmt.Printf("%d %s\n", len(file), args[1])
+			output += fmt.Sprintf("%d ", len(file))
+		case "lines":
+			count := 0
+			for _, b := range file {
+				if b == '\n' {
+					count++
+				}
 			}
-
+			output += fmt.Sprintf("%d ", count)
 		}
 	}
+	return output + args[1]
 }
 
 func processFile(filename string) []byte {
