3c0e5a0773278ee4649a3126d43c4c88852ea85e
shoccho <sfb.shoccho@gmail.com>
2024-10-09 20:15:53 +0600

    Task 01: completed with flags from stdlib

--- 01-wc-tool/main.go
+++ 01-wc-tool/main.go
@@ -1,43 +1,94 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+type flags struct {
+	countByte bool
+	countChar bool
+	countWord bool
+	countLine bool
+}
+
 func main() {
-	args := os.Args[1:]
-	flags := map[string]string{"-c": "bytes", "-l": "lines"}
-	fmt.Println(process(args, flags))
+	flags := processFlags()
+	in, filename := processInput()
+	if filename != "" {
+		defer in.(*os.File).Close()
+	}
+	processOutput(in, flags)
 }
 
-func process(args []string, flags map[string]string) string {
+func processFlags() flags {
+	countByte := flag.Bool("c", false, "Count bytes")
+	countChar := flag.Bool("m", false, "Count charecters")
+	countWord := flag.Bool("w", false, "Count words")
+	countLine := flag.Bool("l", false, "Count liness")
 
-	var output string
-	if value, ok := flags[args[0]]; ok {
-		file := processFile(args[1])
-		switch value {
-		case "bytes":
-			output += fmt.Sprintf("%d ", len(file))
-		case "lines":
-			count := 0
-			for _, b := range file {
-				if b == '\n' {
-					count++
-				}
-			}
-			output += fmt.Sprintf("%d ", count)
+	flag.Prase()
+	if !(*countByte || *countWord || *countLine || *countChar) {
+		*countByte = true
+		*countWord = true
+		*countLine = true
+	}
+
+	return flags{*countByte, *countChar, *countWord, *countLine}
+
+}
+func processInput() (io.Reader, string) {
+	filename := ""
+	if filename = flag.Arg(0); filename != "" {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err.Error())
 		}
+		return file, filename
 	}
-	return output + args[1]
+	return os.Stdin, filename
 }
 
-func processFile(filename string) []byte {
-	file, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return file
+func isWhiteSpace(b rune) bool {
+	return b == ' ' || b == '\n' || b == '\t' || b == '\r'
+}
 
+func processOutput(in io.Reader, f flags) {
+	buf := bufio.NewReader(in)
+	lastByte := rune(0)
+	byteCount, wordCount, charCount, lineCount := 0, 0, 0, 0
+	for {
+		curByte, sz, err := buf.ReadRune()
+		if err == io.EOF {
+			if !isWhiteSpace(lastByte) {
+				wordCount++
+			}
+			break
+		}
+		byteCount += sz
+		charCount++
+		if isWhiteSpace(curByte) && !isWhiteSpace(lastByte) {
+			wordCount++
+		}
+		if curByte == '\n' {
+			lineCount++
+		}
+		lastByte = curByte
+	}
+	if f.countLine {
+		fmt.Print(lineCount, " ")
+	}
+	if f.countChar {
+		fmt.Print(charCount, " ")
+	}
+	if f.countWord {
+		fmt.Print(wordCount, " ")
+	}
+	if f.countByte {
+		fmt.Print(byteCount, " ")
+	}
 }
